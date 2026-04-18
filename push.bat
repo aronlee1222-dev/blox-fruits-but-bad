19192a7d4d9ab38e6f28c23434eea9bb83a43d22
aronlee1222-dev <aronlee1222-dev@users.noreply.github.com>
2026-04-18 15:45:45 +1000

    Keep player on screen

--- push.bat
+++ push.bat
@@ -1,5 +1,3 @@
 set PATH=%PATH%;C:\Program Files\Git\cmd
 "C:\Program Files\GitHub CLI\gh.exe" auth setup-git
-git add .
-git commit -m "Fix syntax error"
-git push -f -u origin main
+git push -u origin main
